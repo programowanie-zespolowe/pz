4ebb64723c367e680f2c28f0c344428d5b495298
Patryk Rzeszotarski <rzeszotarski.pat@gmail.com>
2020-01-14 19:04:18 +0100

    MainActivity, Menu

---
+++ mobile/app/src/main/java/com/example/programowaniezespolowe/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.programowaniezespolowe;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import android.os.Bundle;
+import android.view.MenuItem;
+import com.google.android.material.navigation.NavigationView;
+
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+    private NavigationView nvDrawer;
+
+    private ActionBarDrawerToggle drawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        mDrawer = findViewById(R.id.drawer_layout);
+        drawerToggle = setupDrawerToggle();
+        drawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawer.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+        nvDrawer = findViewById(R.id.nvView);
+        nvDrawer.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                System.out.println(id);
+                switch(menuItem.getItemId()){
+
+                }
+                return true;
+            }
+        });
+
+
+
+    }
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        return new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open,  R.string.drawer_close);
+    }
+
+
+
+
+
+
+}
